ac974a7706f5b4fbebfcfebe0e5bc5b3da0b25a3
Kebin J Martinez T <kebin.martinez.t@gmail.com>
2021-10-11 16:31:25 -0500

    Mejorando el código del archivo Conversor.py implementándole condicionales

--- Curso Básico de Python/Conversor.py
+++ Curso Básico de Python/Conversor.py
@@ -1,7 +1,39 @@
-pesos = input("How many Colombias pesos do you have?: ")
-pesos = float(pesos)
-dollar_value = 3760
-dollars = pesos / dollar_value
-dollars = round(dollars, 2)
-dollars = str(dollars)
-print ("You have $" + dollars + " dollars")
+menu = """
+Welcome to currency converter 💲
+
+     #1 Colombian pesos
+     #2 Argentine pesos
+     #3 Mexican pesos
+
+     Choose an option: 
+"""
+option = input(menu)
+
+if option == "1":
+    pesos = input("How many Colombias pesos do you have?: ")
+    pesos = float(pesos)
+    dollar_value = 3875
+    dollars = pesos / dollar_value
+    dollars = round(dollars, 2)
+    dollars = str(dollars)
+    print ("You have $" + dollars + " dollars")
+
+elif option == "2":
+    pesos = input("How many Argentine pesos do you have?: ")
+    pesos = float(pesos)
+    dollar_value = 65
+    dollars = pesos / dollar_value
+    dollars = round(dollars, 2)
+    dollars = str(dollars)
+    print ("You have $" + dollars + " dollars")
+
+elif option == "3":
+    pesos = input("How many Mexican pesos do you have?: ")
+    pesos = float(pesos)
+    dollar_value = 24
+    dollars = pesos / dollar_value
+    dollars = round(dollars, 2)
+    dollars = str(dollars)
+    print ("You have $" + dollars + " dollars")
+else:
+    print("Please enter one correct option ")
