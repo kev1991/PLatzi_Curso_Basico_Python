8f36df13556701e8236afdb78972c8a0cc8f3edb
Kebin J Martinez T <kebin.martinez.t@gmail.com>
2021-10-15 14:21:17 -0500

    Recorriendo un string con el ciclo for

---
+++ Curso Básico de Python/go_to.py
@@ -0,0 +1,13 @@
+def kev():
+    # name = input ("Write your name : ")
+    # for letter in name:
+    #     print (letter)
+
+    sentence = input ("Write one sentence : ")
+    for character in sentence:
+        print(character.upper())
+    pass
+
+
+if __name__ == "__main__":
+    kev()
