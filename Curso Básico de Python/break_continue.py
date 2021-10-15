8e75246c760e28ba0ee11c5edc4ff33faf7ad1ee
Kebin J Martinez T <kebin.martinez.t@gmail.com>
2021-10-15 14:45:11 -0500

    break_continue

---
+++ Curso Básico de Python/break_continue.py
@@ -0,0 +1,24 @@
+def kev():
+    # for counter in range(1000):
+    #     if counter % 2 != 0:
+    #         continue
+    #     print(counter)
+
+
+    # for kev in range(100):
+    #     print(kev)
+    #     if kev ==50:
+    #         break
+
+
+    text = input("write one text : ")
+    for letter in text:
+        if letter == "o":
+            break
+        print(letter)
+
+    pass
+
+
+if __name__ == "__main__":
+    kev()
